a1cf8162cdeb4ca2df13f1b8365c04db5c05394a
amos-tg <morphouswatery@gmail.com>
2025-08-24 00:49:42 -0400

    added qrexec functions

--- src/client.rs
+++ src/client.rs
@@ -2,6 +2,7 @@ use std::{
     env::var,
     sync::mpsc,
     path::Path,
+    process::{Child, Command},
     fs,
 };
 use notify::{
@@ -10,31 +11,63 @@ use notify::{
     RecursiveMode,
     EventKind,
 };
+use anyhow::anyhow;
+use crate::DRes;
 
-const ZATHURA_PATH_POSTFIX: &str =  
-    ".local/share/zathura";
+pub fn client_main() -> DRes<()> {
+    const ZATHURA_PATH_POSTFIX: &str =  
+        ".local/share/zathura";
+    const HOME_VAR_ERR: &str = 
+        "Error: HOME env var is not present";
 
-pub fn client_main(){
     let path_str = format!(
         "{}/{}",
-        var("HOME").unwrap(),
+        var("HOME").or(Err(anyhow!(HOME_VAR_ERR)))?,
         ZATHURA_PATH_POSTFIX,
     );
     let path = Path::new(&path_str);
 
+    let qrx = Qrexec::new()?;
     let (tx, rx) = mpsc::channel();
-    let mut watcher = recommended_watcher(tx).unwrap();
+    let mut watcher = recommended_watcher(tx)?;
 
-    watcher.watch(&path, RecursiveMode::Recursive).unwrap();
+    watcher.watch(&path, RecursiveMode::Recursive)?;
     loop {
-        let event = rx.recv().unwrap().unwrap();
+        let event = rx.recv()??;
         match event.kind { 
             EventKind::Remove(_) => continue,
             EventKind::Access(_) => continue, 
             _ => (),
         }
         for path in event.paths {
-            let fcont = fs::read_to_string(&path).unwrap();
+            let fcont = fs::read_to_string(&path)?;
+
         }
     } 
+}
+
+struct Qrexec(Child);
+
+impl Qrexec {
+    fn new() -> DRes<Self> {
+        const ZATHURA_BMARK_VM_VAR: &str = "ZATHURA_BMARK_VM";
+        const ZATHURA_BMARK_VM_VAR_ERR: &str = 
+            "Error: ZATHURA_BMARK_VM env var is not present";
+        const RPC_SERVICE_NAME: &str = "qubes.ZathuraMgmt";
+
+        let vm_name = var(ZATHURA_BMARK_VM_VAR)
+            .or(Err(anyhow!(ZATHURA_BMARK_VM_VAR_ERR)))?;
+        Ok(Self(Command::new("qrexec-client-vm")
+            .args([
+                &vm_name,
+                RPC_SERVICE_NAME,
+            ])
+            .spawn()?))
+    }
+}
+
+impl Drop for Qrexec {
+    fn drop(&mut self) {
+        let _ = self.0.kill();   
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 mod client;
 mod server;
 
-use std::env::{args, Args};
+use std::{
+    env::{args, Args},
+    error::Error,
+};
 use crate::{
     client::client_main,
     server::server_main,
 };
+
+type DRes<T> = Result<T, Box<dyn Error>>;
 
 fn main() {
     let model = Model::new(args());
