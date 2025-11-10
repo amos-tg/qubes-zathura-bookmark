a7fb2e8c9c6d3bbfc7451d80c6b221a4f2233bd2
Amos Thacker-Gwaltney <morphouswatery@gmail.com>
2025-11-09 23:24:53 -0500

    Refactored server side protocol
    Fixed the bugs and put everything into a central trait to reduce the
    code duplication I had going on. There is one function left to include
    in Responder::poll_send but I have to write it on the client side so
    I'll just import it from there.

--- src/server.rs
+++ src/server.rs
@@ -2,10 +2,9 @@ use crate::{
     shared_consts::*,
     shared_fn::*,
     conf::Conf,
-    recv_seq,
 };
 use std::{
-    error::Error,
+    io,
     fs::{self, FileType},
     path::{PathBuf, Path},
     num::TryFromIntError,
@@ -14,36 +13,11 @@ use qrexec_binds::{QrexecServer, QIO};
 use anyhow::anyhow;
 
 pub fn server_main(conf: Conf) -> DRes<()> {
-    let mut buf = [0u8; BLEN];
-    let mut qrx = QrexecServer::new();
+    let qrx = QrexecServer::new();
+    let mut responder = Responder::new(qrx);
     loop {
-        request_handler(&mut qrx, &conf, &mut buf)?;
+        responder.poll_send(&conf)?; 
     }
-}
-
-fn request_handler(
-    qrx: &mut QrexecServer,
-    conf: &Conf,
-    rbuf: &mut [u8; BLEN],
-) -> DRes<()> {
-    let nb = qrx.read(rbuf)?;
-    match rbuf {
-        rbuf if rbuf.starts_with(VAR_SEND_SFILE) => 
-            recv_file(qrx, conf, rbuf, nb)?,
-
-        rbuf if rbuf.starts_with(VAR_GET_BOOK) => 
-            send_book(qrx, conf, rbuf)?,
-
-        rbuf if rbuf.starts_with(GET_SFILES) => 
-            send_sfile_tree(qrx, conf, rbuf)?,
-
-        rbuf if rbuf.starts_with(GET_BOOKNAMES) => 
-            send_booknames(qrx, conf, rbuf)?,
-
-        _ => unreachable!(),
-    }
-    
-    return Ok(());
 }
 
 struct Responder {
@@ -53,30 +27,32 @@ struct Responder {
 }
 
 impl Responder {
+    /// First available byte index in Responder.buf not 
+    /// used by the header.
+    const CONT_IDX: usize = 4;
+    
     fn new(qrx: QrexecServer) -> Self {
         Self { qrx, buf: [0u8; BLEN], cursor: 0 }
     }
-
-    //data includes:
-    //  number of reads, 
-    //  payload, 
 
     /// initially the cursor of the Responder object is 
     /// set to the number of bytes from the first read.
     fn poll_send(&mut self, conf: &Conf) -> DRes<()> {
         self.cursor = self.qrx.read(&mut self.buf)?;
         match self.buf[0] {
-            VAR_SEND_SFILE => 
-                recv_file(qrx, conf, rbuf, nb)?,
+            // I'm going to use the function in request
+            // on the client side for this so I don't write
+            // the same thing twice. I will have to write this 
+            // to parse the StateFiles::send functions response.
+            // Ideally it should handle multiple changed files
+            // anyways.
+            //VAR_SEND_SFILE => recv_file(qrx, conf, rbuf, nb)?,
 
-            VAR_GET_BOOK => 
-                send_book(qrx, conf, rbuf)?,
+            Book::ID => Book::send(self, &conf)?,
 
-            GET_SFILES => 
-                send_sfile_tree(qrx, conf, rbuf)?,
+            StateFiles::ID => StateFiles::send(self, &conf)?,
 
-            BookNames::ID => BookNames::send(
-                &mut self, &conf)?,
+            BookNames::ID => BookNames::send(self, &conf)?,
 
             _ => unreachable!(),
         }
@@ -85,10 +61,31 @@ impl Responder {
     }  
 }
 
-// associated ID forces conformity in impl
+enum Content {
+    One(Vec<u8>),
+    More(Vec<Vec<u8>>),
+    None,
+}
+
 trait Response<const ID: u8> {
     fn send(tx: &mut Responder, conf: &Conf) -> DRes<()> {
         let cont = Self::contents(conf, tx)?;
+        return match cont {
+            Content::One(cont) => Self::send_one(tx, cont),
+            Content::More(cont) => Self::send_more(tx, cont),
+            Content::None => Ok(()),
+        };
+    }
+
+    fn send_more(tx: &mut Responder, conts: Vec<Vec<u8>>) -> DRes<()> {
+        for cont in conts {      
+            Self::send_one(tx, cont)?;
+        }
+
+        return Ok(());
+    }
+
+    fn send_one(tx: &mut Responder, cont: Vec<u8>) -> DRes<()> {
         let mut num_reads = Self::set_numreads(tx, cont.len())?;
         let mut max_cursor;
         while num_reads != 0 {
@@ -110,7 +107,7 @@ trait Response<const ID: u8> {
 
         return Ok(());
     }
-
+    
     /// returns the number of reads, sets buf header
     /// with the number of reads required for a response
     /// of msg_len length. Sets the cursor to point to 
@@ -124,7 +121,7 @@ trait Response<const ID: u8> {
         return Ok(nr);
     }
 
-    fn contents(conf: &Conf, tx: &mut Responder) -> DRes<Vec<u8>>;
+    fn contents(conf: &Conf, tx: &mut Responder) -> DRes<Content>;
 }
 
 struct BookNames;
@@ -133,7 +130,7 @@ impl BookNames {
 }
 
 impl Response<{Self::ID}> for BookNames {
-    fn contents(conf: &Conf, _: &mut Responder) -> DRes<Vec<u8>> {
+    fn contents(conf: &Conf, _: &mut Responder) -> DRes<Content> {
         let mut bnames: Vec<u8> = vec!();
         let bdir_entries = fs::read_dir(&conf.book_dir)?;
         for bentry in bdir_entries {
@@ -149,111 +146,110 @@ impl Response<{Self::ID}> for BookNames {
             bnames.push(NONE);
         }
 
-        return Ok(bnames);
+        return Ok(Content::One(bnames));
     }
 }
 
 struct Book;
 impl Book {
     const ID: u8 = b'2';
+
+    fn find_book(book_dir: &Path, bname: &str) -> io::Result<Option<PathBuf>> {
+        let path = book_dir.join(bname).to_owned();
+        if fs::exists(&path)? {
+            return Ok(Some(path));
+        } else {
+            for file in fs::read_dir(book_dir)? {
+                let file = file?;
+                if file.file_type()?.is_dir() {
+                    return Self::find_book(&file.path(), bname);
+                } 
+            }
+            return Ok(None);
+        }                
+    }
 }
 
 impl Response<{Book::ID}> for Book {
-    fn contents(conf: &Conf, tx: &mut Responder) -> DRes<Vec<u8>> {
-        tx.buf
-         
-        return Ok();
+    /// returns the book cont if it exists in book dir, else
+    /// returns an empty vector if it doesn't exist.
+    fn contents(conf: &Conf, tx: &mut Responder) -> DRes<Content> {
+        let bname = str::from_utf8(&tx.buf[1..tx.cursor])?;
+        let bpath = Self::find_book(Path::new(&conf.book_dir), bname)?;
+        if let Some(bpath) = bpath { 
+            return Ok(Content::One(fs::read(&bpath)?));
+        } else {
+            return Ok(Content::None);
+        }
     }
 } 
 
-fn send_book(
-    qrx: &mut QrexecServer,
-    conf: &Conf,
-    rbuf: &mut [u8; BLEN],
-) -> DRes<()> {
-    let delim1 = find_delim(rbuf, b':')
-        .ok_or(anyhow!(MSG_FORMAT_ERR))?;
-    let delim2 = find_delim(rbuf, b';') 
-        .ok_or(anyhow!(MSG_FORMAT_ERR))?;
 
-    let bname = str::from_utf8(&rbuf[(delim1 + 1)..delim2])?;
-    let bpath = find_book(Path::new(&conf.book_dir), bname)?;
+struct StateFiles;
+impl StateFiles {
+    const ID: u8 = b'3'; 
 
-    send_file(qrx, &bpath, rbuf, false)?;
-
-    return Ok(());
-}
-
-fn find_book(book_dir: &Path, bname: &str) -> DRes<PathBuf> {
-    let path = book_dir.join(bname).to_owned();
-    if fs::exists(&path)? {
-        return Ok(path);
-    } else {
-        for file in fs::read_dir(book_dir)? {
+    fn recurse_files(
+        read_dir: fs::ReadDir,
+        files: &mut Vec<(PathBuf, FileType)>,
+    ) -> DRes<()> {
+        for file in read_dir {
             let file = file?;
-            if file.file_type()?.is_dir() {
-                return find_book(&file.path(), bname);
-            } 
+            let path = file.path();
+            let file_type = file.file_type()?;
+
+            if file_type.is_file() {
+                files.push((path, file_type));
+            } else if file_type.is_dir() {
+                Self::recurse_files(
+                    fs::read_dir(&path)?,
+                    files)?;
+                files.push((path, file_type));
+            } else if file_type.is_symlink() {
+                Err(anyhow!(SYMLINK_ERR))?;
+            }
         }
-        return Err(anyhow!(BOOK_UNAVAILABLE_ERR))?;
-    }                
-}
 
-fn send_sfile_tree(
-    qrx: &mut QrexecServer,
-    conf: &Conf,
-    rbuf: &mut [u8; BLEN],
-) -> DRes<()> {
-    let mut file_paths: Vec<(PathBuf, FileType)> = vec!();
-    let mut cursor = 0usize;
-
-    recurse_files(
-        fs::read_dir(&conf.state_dir)?,
-        &mut file_paths)?;
-
-    if file_paths.is_empty() {
-        qrx.write(NONE)?;
-        recv_seq!(qrx, rbuf);
         return Ok(());
     }
-
-    // this is wrong 
-    let num_files_bytes = u32::to_ne_bytes(file_paths.len().try_into()?);
-
-    cursor += set_slice(rbuf, VAR_SEND_NUM_SFILES);
-    cursor += set_slice(&mut rbuf[cursor..], &num_files_bytes);
-
-    qrx.write(&rbuf[..cursor])?;
-    recv_seq!(qrx, rbuf);
-
-    for (fpath, ftype) in file_paths {
-        let ftype = ftype.is_dir();
-        send_file(qrx, fpath.as_path(), rbuf, ftype)?;
-    }
-
-    return Ok(());
 }
 
-fn recurse_files(
-    read_dir: fs::ReadDir,
-    files: &mut Vec<(PathBuf, FileType)>,
-) -> DRes<()> {
-    for file in read_dir {
-        let file = file?;
-        let path = file.path();
-        let file_type = file.file_type()?;
+impl Response<{Self::ID}> for StateFiles {
+    /// directories are denoted by zero contents, ie. the accompanying 
+    /// vector of like ordered names will not have a content indice set
+    /// Paths and Contents are sent in this order (the paths sent are 
+    /// stripped of the prefix conf.state_dir): 
+    /// * 1: Paths, 
+    /// * 2: Contents,
+    fn contents(conf: &Conf, _: &mut Responder) -> DRes<Content> {
+        let mut file_paths: Vec<(PathBuf, FileType)> = vec!();
+        let mut contents: Vec<u8> = vec!();
+        let mut fpaths: Vec<u8> = vec!();
 
-        if file_type.is_file() {
-            files.push((path, file_type));
-        } else if file_type.is_dir() {
-            recurse_files(
-                fs::read_dir(&path)?,
-                files)?;
-            files.push((path, file_type));
-        } else if file_type.is_symlink() {
-            Err(anyhow!(SYMLINK_ERR))?;
+        Self::recurse_files(
+            fs::read_dir(&conf.state_dir)?,
+            &mut file_paths)?;
+
+        if file_paths.is_empty() {
+            return Ok(Content::None);
         }
-    }
-    
-    return Ok(());
+
+        for (path, ftype) in file_paths {
+            let fpath_bytes = path
+                .as_path()
+                .strip_prefix(&conf.state_dir)?
+                .as_os_str()
+                .as_encoded_bytes();
+
+            if ftype.is_dir() {
+                fpaths.extend_from_slice(fpath_bytes);
+            } else if ftype.is_file() {
+                fpaths.extend_from_slice(fpath_bytes);
+                let fcont_bytes = fs::read(path)?;
+                contents.extend_from_slice(&fcont_bytes);
+            }
+        }
+
+        return Ok(Content::More(vec![fpaths, contents]));
+    } 
 }
